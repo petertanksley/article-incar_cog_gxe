a4efc69fb092aa962c6184ec40e6a38d8e395000
petertanksley <peter.tanksley@gmail.com>
2023-05-24 00:08:21 -0500

    Added stratified analyses and tables

--- code/3_analysis.R
+++ code/3_analysis.R
@@ -1,7 +1,8 @@
 source("0_packages.R")
 
 hrs_full <- import("hrs_full_analytic.rds") 
-hrs_pgs <- import("hrs_pgs_analytic.rds") 
+hrs_pgs_eur <- import("hrs_pgs_analytic.rds") %>% filter(race_ethn=="White")
+hrs_pgs_afr <- import("hrs_pgs_analytic.rds") %>% filter(race_ethn=="Black")
 
 #=H1: prevalence===============================================================
 
@@ -49,64 +50,122 @@ main_results <- bind_rows(res_10,
 #export results
 rio::export(main_results, "../output/results/tab3_main_results.csv")
 
-# #=PGS analysis================================================================
-# 
-# #set model formulas 
-# m0_base <- formula("cog_2cat_num ~ 
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m1_pgs <- formula("cog_2cat_num ~ scale(ad_pgs_resid) + 
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m2_apoe <- formula("cog_2cat_num ~ factor(apoe_info99_4ct) +
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m3_pgs_apoe <- formula("cog_2cat_num ~ scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m4_incar <- formula("cog_2cat_num ~ factor(incar_ever) + 
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m5_main_eff <- formula("cog_2cat_num ~ factor(incar_ever) + scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# m6_int_eff <- formula("cog_2cat_num ~ factor(incar_ever)*scale(ad_pgs_resid) + factor(incar_ever)*factor(apoe_info99_4ct) +
-#                    factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
-#                    (1|hhidpn)")
-# 
-# #run models 
-# m0 <- glmer(m0_base,     data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m1 <- glmer(m1_pgs,      data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m2 <- glmer(m2_apoe,     data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m3 <- glmer(m3_pgs_apoe, data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m4 <- glmer(m4_incar,    data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m5 <- glmer(m5_main_eff, data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# m6 <- glmer(m6_int_eff,  data=hrs, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-# 
-# export(c("m0","m1","m2","m3","m4","m5","m6"),
-#             "../output/results/tab3_main_results.rdata")
-# 
-# 
-# #table models
-# res_0 <- tidy(m0, exponentiate=TRUE) %>% mutate(model="model base") %>% mutate(glance(m0)) 
-# res_1 <- tidy(m1, exponentiate=TRUE) %>% mutate(model="model 1")    %>% mutate(glance(m1)) 
-# res_2 <- tidy(m2, exponentiate=TRUE) %>% mutate(model="model 2")    %>% mutate(glance(m2))
-# res_3 <- tidy(m3, exponentiate=TRUE) %>% mutate(model="model 3")    %>% mutate(glance(m3))
-# res_4 <- tidy(m4, exponentiate=TRUE) %>% mutate(model="model 4")    %>% mutate(glance(m4))
-# res_5 <- tidy(m5, exponentiate=TRUE) %>% mutate(model="model 5")    %>% mutate(glance(m5))
-# res_6 <- tidy(m6, exponentiate=TRUE) %>% mutate(model="model 6")    %>% mutate(glance(m6))
-# 
-# main_results <- bind_rows(res_0,
-#                           res_1,
-#                           res_2,
-#                           res_3,
-#                           res_4,
-#                           res_5,
-#                           res_6)
-# 
-# #export results
-# export(main_results, "../output/results/tab3_main_results.csv")
-# 
+#=PGS analysis (EUR)================================================================
+
+#set model formulas
+m20_base <- formula("cog_2cat_num ~
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m21_pgs <- formula("cog_2cat_num ~ scale(ad_pgs_resid) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m22_apoe <- formula("cog_2cat_num ~ factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m23_pgs_apoe <- formula("cog_2cat_num ~ scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m24_incar <- formula("cog_2cat_num ~ factor(incar_ever) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m25_main_eff <- formula("cog_2cat_num ~ factor(incar_ever) + scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m26_int_eff <- formula("cog_2cat_num ~ factor(incar_ever)*scale(ad_pgs_resid) + factor(incar_ever)*factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+
+#run models
+m20 <- glmer(m20_base,     data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m21 <- glmer(m21_pgs,      data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m22 <- glmer(m22_apoe,     data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m23 <- glmer(m23_pgs_apoe, data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m24 <- glmer(m24_incar,    data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m25 <- glmer(m25_main_eff, data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m26 <- glmer(m26_int_eff,  data=hrs_pgs_eur, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+
+export(c("m20","m21","m22","m23","m24","m25","m26"),
+            "../output/results/tab_s1_eur.rdata")
+
+
+#table models
+res_20 <- tidy(m20, exponentiate=TRUE) %>% mutate(model="model base") %>% mutate(glance(m20))
+res_21 <- tidy(m21, exponentiate=TRUE) %>% mutate(model="model 1")    %>% mutate(glance(m21))
+res_22 <- tidy(m22, exponentiate=TRUE) %>% mutate(model="model 2")    %>% mutate(glance(m22))
+res_23 <- tidy(m23, exponentiate=TRUE) %>% mutate(model="model 3")    %>% mutate(glance(m23))
+res_24 <- tidy(m24, exponentiate=TRUE) %>% mutate(model="model 4")    %>% mutate(glance(m24))
+res_25 <- tidy(m25, exponentiate=TRUE) %>% mutate(model="model 5")    %>% mutate(glance(m25))
+res_26 <- tidy(m26, exponentiate=TRUE) %>% mutate(model="model 6")    %>% mutate(glance(m26))
+
+main_results2 <- bind_rows(res_20,
+                          res_21,
+                          res_22,
+                          res_23,
+                          res_24,
+                          res_25,
+                          res_26)
+
+#export results
+export(main_results2, "../output/results/tab_s1_eur.csv")
+
+#=PGS analysis (AFR)================================================================
+
+#set model formulas
+m30_base <- formula("cog_2cat_num ~
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m31_pgs <- formula("cog_2cat_num ~ scale(ad_pgs_resid) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m32_apoe <- formula("cog_2cat_num ~ factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m33_pgs_apoe <- formula("cog_2cat_num ~ scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m34_incar <- formula("cog_2cat_num ~ factor(incar_ever) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m35_main_eff <- formula("cog_2cat_num ~ factor(incar_ever) + scale(ad_pgs_resid) + factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+m36_int_eff <- formula("cog_2cat_num ~ factor(incar_ever)*scale(ad_pgs_resid) + factor(incar_ever)*factor(apoe_info99_4ct) +
+                   factor(sex) + scale(age) + scale(social_origins) + factor(stroke_ever) + factor(study) +
+                   (1|hhidpn)")
+
+#run models
+m30 <- glmer(m30_base,     data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m31 <- glmer(m31_pgs,      data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m32 <- glmer(m32_apoe,     data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m33 <- glmer(m33_pgs_apoe, data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m34 <- glmer(m34_incar,    data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m35 <- glmer(m35_main_eff, data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+m36 <- glmer(m36_int_eff,  data=hrs_pgs_afr, family=poisson(link="log"), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+
+export(c("m30","m31","m32","m33","m34","m35","m36"),
+       "../output/results/tab_s1_afr.rdata")
+
+
+#table models
+res_30 <- tidy(m30, exponentiate=TRUE) %>% mutate(model="model base") %>% mutate(glance(m30))
+res_31 <- tidy(m31, exponentiate=TRUE) %>% mutate(model="model 1")    %>% mutate(glance(m31))
+res_32 <- tidy(m32, exponentiate=TRUE) %>% mutate(model="model 2")    %>% mutate(glance(m32))
+res_33 <- tidy(m33, exponentiate=TRUE) %>% mutate(model="model 3")    %>% mutate(glance(m33))
+res_34 <- tidy(m34, exponentiate=TRUE) %>% mutate(model="model 4")    %>% mutate(glance(m34))
+res_35 <- tidy(m35, exponentiate=TRUE) %>% mutate(model="model 5")    %>% mutate(glance(m35))
+res_36 <- tidy(m36, exponentiate=TRUE) %>% mutate(model="model 6")    %>% mutate(glance(m36))
+
+main_results3 <- bind_rows(res_30,
+                           res_31,
+                           res_32,
+                           res_33,
+                           res_34,
+                           res_35,
+                           res_36)
+
+#export results
+export(main_results3, "../output/results/tab_s1_afr.csv")
+
 #=END==========================================================================
 
 
--- code/4_tables.R
+++ code/4_tables.R
@@ -2,14 +2,14 @@ source("0_packages.R")
 
 hrs <- import("hrs_full_analytic.rds")
 
+#get case/observation numbers
+cases <- n_distinct(hrs$hhidpn)
+obs <- nrow(hrs)
+
 #set table theme
 set_gtsummary_theme(theme_gtsummary_compact())
 
 #=Table 1 - Descriptives=======================================================
-
-#get case/observation numbers
-cases <- n_distinct(hrs$hhidpn)
-obs <- nrow(hrs)
 
 #generate descriptives table
 table_1 <- hrs %>% 
@@ -140,5 +140,172 @@ tab3_all %>%
 # tab3_all %>% 
 #   as_flex_table() %>%
 #   flextable::save_as_docx(path="../output/results/tab3_main_res.docx")
+
+#=Table S1 - polygenic risk (EUR)==============================================
+
+hrs_pgs_eur <- import("hrs_pgs_analytic.rds") %>% filter(race_ethn=="White")
+
+#get case/observation numbers
+cases <- n_distinct(hrs_pgs_eur$hhidpn)
+obs <- nrow(hrs_pgs_eur)
+
+
+poly_eur_results <- import_list("../output/results/tab_s1_eur.rdata") 
+list2env(poly_eur_results, .GlobalEnv)
+
+#function to calculate delta r2
+delta_r2 <- function(model, base=m0){
+  r2_0 <- r2(m0) %>% unlist()
+  r2_plus1 <- r2(model) %>% unlist()
+  delta_r2 <- r2_plus1[2] - r2_0[2]
+  print(delta_r2)
+}
+
+
+tab_s1 <- tbl_regression(m21, exponentiate=TRUE, 
+                         include=c("scale(ad_pgs_resid)"),
+                         label=c("scale(ad_pgs_resid)"="PGS AD"))
+tab_s2 <- tbl_regression(m22, exponentiate=TRUE, 
+                        include=c("factor(apoe_info99_4ct)"),
+                        label=c("factor(apoe_info99_4ct)"="APOE-4 allele count"))
+tab_s3 <- tbl_regression(m23, exponentiate=TRUE, 
+                         include=c("scale(ad_pgs_resid)","factor(apoe_info99_4ct)"),
+                         label=c("scale(ad_pgs_resid)"="PGS AD",
+                                 "factor(apoe_info99_4ct)"="APOE-4 allele count"))
+tab_s4 <- tbl_regression(m24, exponentiate=TRUE, 
+                        include=c("factor(incar_ever)"),
+                        label=c("factor(incar_ever)"="Lifetime incarceration"))
+tab_s5 <- tbl_regression(m25, exponentiate=TRUE, 
+                        include=c("factor(incar_ever)","scale(ad_pgs_resid)","factor(apoe_info99_4ct)"),
+                        label=list("scale(ad_pgs_resid)"="PGS AD",
+                                   "factor(apoe_info99_4ct)"="APOE-4 allele count",
+                                   "factor(incar_ever)"="Lifetime incarceration"))
+tab_s6 <- tbl_regression(m26, exponentiate=TRUE, 
+                        include=c("factor(incar_ever)","scale(ad_pgs_resid)","factor(apoe_info99_4ct)",
+                                  "factor(incar_ever):scale(ad_pgs_resid)",
+                                  "factor(incar_ever):factor(apoe_info99_4ct)"),
+                        label=list("scale(ad_pgs_resid)"="PGS AD",
+                                   "factor(apoe_info99_4ct)"="APOE-4 allele count",
+                                   "factor(incar_ever)"="Lifetime incarceration",
+                                   "factor(incar_ever):factor(apoe_info99_4ct)"="PGS AD",
+                                   "factor(incar_ever):factor(apoe_info99_4ct)"="Lifetime Incarceration*APOE-4 allele count"
+                                   ))
+
+tab_s1_mods <- list(tab_s1,
+                    tab_s2,
+                    tab_s3,
+                    tab_s4,
+                    tab_s5,
+                    tab_s6)
+
+#write function to update gtsumamry tables
+tab_updates <- function(x){
+  x %>% 
+    add_significance_stars(hide_ci = FALSE, hide_p = TRUE, hide_se = TRUE) %>% 
+    remove_row_type(type = "reference") %>% 
+    modify_table_body(~ .x %>%
+                        dplyr::mutate(ci = ifelse(!is.na(ci), paste0("[", ci, "]"), "")))
+}
+
+tab_s1_mods_update <- lapply(tab_s1_mods, tab_updates)
+
+tab_s1_all <- tbl_merge(tab_s1_mods_update, tab_spanner = paste("Model", 1:6, sep = " ")) %>% 
+  modify_header(label = "**Variable**") %>% 
+  modify_caption(glue('**Table S1**. Mixed effect Poisson regression of cognitive status on mono-/polygenic risk and lifetime incarceration among White HRS participants (Observations={format(obs, big.mark=",")}; Cases={format(cases, big.mark=",")})')) %>% 
+  modify_footnote(label ~ "All models also adjusted for age, sex, race/ethnicity, high school completion, smoking history, stroke history, and social origins index.") 
+tab_s1_all
+
+tab_s1_all %>% 
+  as_gt() %>% 
+  gtsave("../output/results/tab_s1_eur.html")
+
+# tab_s1_all %>% 
+#   as_flex_table() %>%
+#   flextable::save_as_docx(path="../output/results/tab_s1_eur.docx")
+
+
+
+#=Table S1 - polygenic risk (AFR)==============================================
+
+hrs_pgs_afr <- import("hrs_pgs_analytic.rds") %>% filter(race_ethn=="Black")
+
+#get case/observation numbers
+cases <- n_distinct(hrs_pgs_afr$hhidpn)
+obs <- nrow(hrs_pgs_afr)
+
+
+poly_afr_results <- import_list("../output/results/tab_s1_afr.rdata") 
+list2env(poly_afr_results, .GlobalEnv)
+
+#function to calculate delta r2
+delta_r2 <- function(model, base=m0){
+  r2_0 <- r2(m0) %>% unlist()
+  r2_plus1 <- r2(model) %>% unlist()
+  delta_r2 <- r2_plus1[2] - r2_0[2]
+  print(delta_r2)
+}
+
+
+tab_s1 <- tbl_regression(m31, exponentiate=TRUE, 
+                         include=c("scale(ad_pgs_resid)"),
+                         label=c("scale(ad_pgs_resid)"="PGS AD"))
+tab_s2 <- tbl_regression(m32, exponentiate=TRUE, 
+                         include=c("factor(apoe_info99_4ct)"),
+                         label=c("factor(apoe_info99_4ct)"="APOE-4 allele count"))
+tab_s3 <- tbl_regression(m33, exponentiate=TRUE, 
+                         include=c("scale(ad_pgs_resid)","factor(apoe_info99_4ct)"),
+                         label=c("scale(ad_pgs_resid)"="PGS AD",
+                                 "factor(apoe_info99_4ct)"="APOE-4 allele count"))
+tab_s4 <- tbl_regression(m34, exponentiate=TRUE, 
+                         include=c("factor(incar_ever)"),
+                         label=c("factor(incar_ever)"="Lifetime incarceration"))
+tab_s5 <- tbl_regression(m35, exponentiate=TRUE, 
+                         include=c("factor(incar_ever)","scale(ad_pgs_resid)","factor(apoe_info99_4ct)"),
+                         label=list("scale(ad_pgs_resid)"="PGS AD",
+                                    "factor(apoe_info99_4ct)"="APOE-4 allele count",
+                                    "factor(incar_ever)"="Lifetime incarceration"))
+tab_s6 <- tbl_regression(m36, exponentiate=TRUE, 
+                         include=c("factor(incar_ever)","scale(ad_pgs_resid)","factor(apoe_info99_4ct)",
+                                   "factor(incar_ever):scale(ad_pgs_resid)",
+                                   "factor(incar_ever):factor(apoe_info99_4ct)"),
+                         label=list("scale(ad_pgs_resid)"="PGS AD",
+                                    "factor(apoe_info99_4ct)"="APOE-4 allele count",
+                                    "factor(incar_ever)"="Lifetime incarceration",
+                                    "factor(incar_ever):factor(apoe_info99_4ct)"="PGS AD",
+                                    "factor(incar_ever):factor(apoe_info99_4ct)"="Lifetime Incarceration*APOE-4 allele count"
+                         ))
+
+tab_s2_mods <- list(tab_s1,
+                    tab_s2,
+                    tab_s3,
+                    tab_s4,
+                    tab_s5,
+                    tab_s6)
+
+#write function to update gtsumamry tables
+tab_updates <- function(x){
+  x %>% 
+    add_significance_stars(hide_ci = FALSE, hide_p = TRUE, hide_se = TRUE) %>% 
+    remove_row_type(type = "reference") %>% 
+    modify_table_body(~ .x %>%
+                        dplyr::mutate(ci = ifelse(!is.na(ci), paste0("[", ci, "]"), "")))
+}
+
+tab_s2_mods_update <- lapply(tab_s2_mods, tab_updates)
+
+tab_s2_all <- tbl_merge(tab_s1_mods_update, tab_spanner = paste("Model", 1:6, sep = " ")) %>% 
+  modify_header(label = "**Variable**") %>% 
+  modify_caption(glue('**Table S2**. Mixed effect Poisson regression of cognitive status on mono-/polygenic risk and lifetime incarceration among Black HRS participants (Observations={format(obs, big.mark=",")}; Cases={format(cases, big.mark=",")})')) %>% 
+  modify_footnote(label ~ "All models also adjusted for age, sex, race/ethnicity, high school completion, smoking history, stroke history, and social origins index.") 
+tab_s2_all
+
+tab_s2_all %>% 
+  as_gt() %>% 
+  gtsave("../output/results/tab_s2_afr.html")
+
+# tab_s2_all %>% 
+#   as_flex_table() %>%
+#   flextable::save_as_docx(path="../output/results/tab_s2_afr.docx")
+
 
 
